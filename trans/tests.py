467d0e5f4cc9d09ade88e1cc956b7096fde685c3
Yoji <yoji.ozawa@gmail.com>
2017-06-30 20:01:59 +0000

    writing test

--- trans/tests.py
+++ trans/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import AnonymousUser, User
 
-# Create your tests here.
+from trans.models import PmethodGroup
+
+USER='admin'
+PASS='hogehoge'
+
+class TransTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username=USER, email='admin@test.com',\
+                                             password=PASS)
+
+    def test_login(self):
+        c = Client()
+        response = c.post('/login/', {'username': USER, 'password': PASS})
+        #print(response.status_code)
+        print(response.content)
+        self.assertEqual(response.status_code, 200)
+
+    def test_show_trans(self):
+        c = Client()
+        c.login(username=USER, password=PASS)
+        response = c.post('/t/pmethod', {'username': USER, 'password': PASS})
+        response = c.post('/t/', {'username': USER, 'password': PASS})
+        #print(response.status_code)
+        #print(response.content)
+        self.assertEqual(response.status_code, 200)
+
+        
