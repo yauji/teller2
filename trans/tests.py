900d4eb97b15e0a54fa3712fba0821ba759fcb5b
Yoji <yoji.ozawa@gmail.com>
2017-06-30 20:31:26 +0000

    writing test

--- trans/tests.py
+++ trans/tests.py
@@ -79,5 +79,28 @@ class TransTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         
 
+class TransTestCase2(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username=USER, email='admin@test.com',\
+                                             password=PASS)
+
+    def test_login(self):
+        c = Client()
+        response = c.post('/')
+        #response = c.post('/login/', {'username': USER, 'password': PASS})
+        #print(response.status_code)
+        #print(response.content)
+        self.assertEqual(response.status_code, 302)
+
+    def test_show_trans(self):
+        c = Client()
+        c.login(username=USER, password=PASS)
+        #response = c.post('/t/pmethod', {'username': USER, 'password': PASS})
+        response = c.post('/t/', {'username': USER, 'password': PASS})
+        #print(response.status_code)
+        #print(response.content)
+        self.assertEqual(response.status_code, 200)
+        
+from django.contrib.auth.models import A        
         
         
