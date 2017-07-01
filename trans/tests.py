c6964450e34ee01fb0e818fab208713929cdff81
Yoji <yoji.ozawa@gmail.com>
2017-07-01 01:01:55 +0000

    writing test

--- trans/tests.py
+++ trans/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test import Client
 from django.contrib.auth.models import AnonymousUser, User
 
-from trans.models import PmethodGroup, Pmethod
+from trans.models import PmethodGroup, Pmethod, CategoryGroup, Category, Trans
 
 USER='admin'
 PASS='hogehoge'
@@ -84,23 +84,40 @@ class TransTestCase2(TestCase):
         self.user = User.objects.create_user(username=USER, email='admin@test.com',\
                                              password=PASS)
 
-    def test_login(self):
-        c = Client()
-        response = c.post('/')
-        #response = c.post('/login/', {'username': USER, 'password': PASS})
-        #print(response.status_code)
-        #print(response.content)
-        self.assertEqual(response.status_code, 302)
+        pmg = PmethodGroup.objects.create(name='pmg1')
+        Pmethod.objects.create(group=pmg, name='pm1')
 
-    def test_show_trans(self):
+        cg = CategoryGroup.objects.create(name='cg1')
+        Category.objects.create(group=cg, name='c1')
+
+
+    def test_add_ok01(self):
         c = Client()
         c.login(username=USER, password=PASS)
-        #response = c.post('/t/pmethod', {'username': USER, 'password': PASS})
-        response = c.post('/t/', {'username': USER, 'password': PASS})
+
+        pms = Pmethod.objects.all()
+        #print(pms)
+        cs = Category.objects.all()
+
+
+        response = c.post('/t/add', {'date': '2017/01/01', 'name': 'item1',\
+                                     'c':cs[0].id,\
+                                     'pm':pms[0].id,\
+                                     'expense':100,\
+                                     'memo':'memo1',\
+        })
         #print(response.status_code)
         #print(response.content)
         self.assertEqual(response.status_code, 200)
+
+        ts = Trans.objects.all()
+        self.assertEqual(len(ts), 1)
+        self.assertEqual(ts[0].name, 'item1')
+
+
+        #hoge
+
         
-from django.contrib.auth.models import A        
+
         
         
