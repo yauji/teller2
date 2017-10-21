d485f1626337101cee7cb112bf2ed15dc2816ee9
Yoji <yoji.ozawa@gmail.com>
2017-10-21 14:05:48 +0000

    changed to postgres.

--- teller2/settings.py
+++ teller2/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'teller2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'postgres',
+         'USER': 'postgres',
+         'PASSWORD' : 'postgres',
+         'HOST' : '192.168.10.131',
+         'PORT' : 5432,
+     }
+ }
 
 
 # Password validation
