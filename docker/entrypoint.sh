317e3ae72ad8381a0341520adc778f415da4dde5
Yoji <yoji.ozawa@gmail.com>
2017-11-24 14:53:41 +0000

    updated dockerfile.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#cd teller2
+python3 ./teller2/manage.py make migrations trans
+python3 ./teller2/manage.py migrate
+python3 ./teller2/manage.py runserver 0.0.0.0:8000
+
+#exec "$@"
--- teller2/settings.py
+++ teller2/settings.py
@@ -75,7 +75,6 @@ WSGI_APPLICATION = 'teller2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -89,11 +88,13 @@ DATABASES = {
          'NAME': 'teller2',
          'USER': 'postgres',
          'PASSWORD' : 'postgres',
-         'HOST' : '192.168.10.131',
+         'HOST' : 'db',
          'PORT' : 5432,
      }
  }
+#         'HOST' : '192.168.10.131',
 """
+
 
 
 
