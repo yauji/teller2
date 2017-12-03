7c9579d261baa59fe421787734299083ab91b4de
Yoji <yoji.ozawa@gmail.com>
2017-12-03 12:42:18 +0000

    added coolect static

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,7 @@
 
 python3 ./teller2/manage.py makemigrations trans
 python3 ./teller2/manage.py migrate
-python3 ./teller2/manage.py runserver 0.0.0.0:8000
+#python3 ./teller2/manage.py runserver 0.0.0.0:8000
+uwsgi --ini uwsgi.ini
 
 exec "$@"
