3a8711ac00d1fd4e1a17751b3bc29ff9aac8a90f
Yoji <yoji.ozawa@gmail.com>
2017-12-04 10:59:36 +0000

    uwsgi http.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 
-python3 ./teller2/manage.py makemigrations trans
-python3 ./teller2/manage.py migrate
+python3 ./manage.py makemigrations trans
+python3 ./manage.py migrate
+#python3 ./teller2/manage.py makemigrations trans
+#python3 ./teller2/manage.py migrate
 #python3 ./teller2/manage.py runserver 0.0.0.0:8000
+#cd ./teller2
 uwsgi --ini uwsgi.ini
 
 exec "$@"
