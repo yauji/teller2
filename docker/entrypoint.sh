278f9e1d76ec29a675c81e0bb5b2112091fcdc2e
Yoji <yoji.ozawa@gmail.com>
2017-12-04 11:58:13 +0000

    added nginx.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,9 @@ python3 ./manage.py migrate
 #python3 ./teller2/manage.py migrate
 #python3 ./teller2/manage.py runserver 0.0.0.0:8000
 #cd ./teller2
+
+/etc/init.d/nginx start
+
 uwsgi --ini uwsgi.ini
 
 exec "$@"
