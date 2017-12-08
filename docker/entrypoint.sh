8f35ffa5eb5b6d73069afd517ac85c2413c8b049
Yoji <yoji.ozawa@gmail.com>
2017-12-08 14:01:45 +0000

    .

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-
-python3 ./manage.py makemigrations trans
-python3 ./manage.py migrate
-#python3 ./teller2/manage.py makemigrations trans
-#python3 ./teller2/manage.py migrate
+#python3 ./manage.py makemigrations trans
+#python3 ./manage.py migrate
+python3 ./teller2/manage.py makemigrations trans
+python3 ./teller2/manage.py migrate
 #python3 ./teller2/manage.py runserver 0.0.0.0:8000
-#cd ./teller2
 
 /etc/init.d/nginx start
 
+cd ./teller2
 uwsgi --ini uwsgi.ini
 
 exec "$@"
