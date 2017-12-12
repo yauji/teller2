77b20719ca3593aeecb6e7d0e3ad1f930517401b
Yoji <yoji.ozawa@gmail.com>
2017-12-12 10:54:56 +0000

    .

--- teller2/settings.py
+++ teller2/settings.py
@@ -79,7 +79,6 @@ WSGI_APPLICATION = 'teller2.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 """
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -97,6 +96,7 @@ DATABASES = {
          'PORT' : 5432,
      }
  }
+
 
 #         'HOST' : 'db',
 #         'NAME': 'teller2',
